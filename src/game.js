e930039c380aaa9d67e0b3a268f1395b42e691fa
emesefedev <emesefedev@gmail.com>
2025-01-06 16:38:49 +0100

    feature: game logic works

--- src/game.js
+++ src/game.js
@@ -17,42 +17,32 @@ let isGameOver = false;
 
 startGame();
 
-let i = 0;
 while (!isGameOver) {
-  console.log(
-    `${currentPlayer.name} attacks ${enemy.name} in (${Math.floor(i / 2)}, ${0})`,
-  );
-  currentPlayer.attack(enemy, Math.floor(i / 2), 0);
+  let x = prompt("Enter coord X:");
+  let y = prompt("Enter coord Y:");
+  console.log(`${currentPlayer.name} attacks ${enemy.name} in ${x}, ${y})`);
+  currentPlayer.attack(enemy, x, y);
+
+  enemy.printGameBoard();
 
   isGameOver = enemy.isGameOver();
   if (!isGameOver) changeTurn();
-  i++;
 }
 
 console.log(`${currentPlayer.name} wins!`);
 
-playerA.printGameBoard();
-console.log("");
-playerB.printGameBoard();
-
-// playerA.attack(playerB, 0, 0);
-// playerA.attack(playerB, 2, 0);
-// playerA.attack(playerB, 1, 0);
-
-// playerB.printGameBoard();
-
 function startGame() {
   playerA.placeShip(ship2, 5, 7, "h");
-  //   playerA.placeShip(ship3A, 4, 7, "h");
-  //   playerA.placeShip(ship3B, 2, 1, "v");
-  //   playerA.placeShip(ship4, 0, 5, "h");
-  //   playerA.placeShip(ship5, 8, 3, "h");
+  playerA.placeShip(ship3A, 4, 7, "h");
+  playerA.placeShip(ship3B, 2, 1, "v");
+  playerA.placeShip(ship4, 0, 5, "h");
+  playerA.placeShip(ship5, 8, 3, "h");
 
   playerB.placeShip(ship2, 0, 0, "v");
-  //   playerB.placeShip(ship3A, 0, 1, "v");
-  //   playerB.placeShip(ship3B, 0, 2, "v");
-  //   playerB.placeShip(ship4, 0, 3, "v");
-  //   playerB.placeShip(ship5, 0, 4, "v");
+  playerB.placeShip(ship3A, 0, 1, "v");
+  playerB.placeShip(ship3B, 0, 2, "v");
+  playerB.placeShip(ship4, 0, 3, "v");
+  playerB.placeShip(ship5, 0, 4, "v");
 
   currentPlayer = playerA;
   enemy = playerB;
